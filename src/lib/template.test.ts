33ff4f37f9ea80e5462192d9ad90fc4fd3784f95
zlq4863947 <zlq4863947@gmail.com>
2017-09-23 04:37:17 +0900

    板块数据追加

--- src/lib/template.test.ts
+++ src/lib/template.test.ts
@@ -1,23 +1,20 @@
 import { Template } from './template';
 import * as assert from 'power-assert';
 import { Sequelize} from 'sequelize-typescript';
+import Sector from './models/sector';
 
-const sequelize = new Sequelize({
-  host: '45.32.48.129',
-  database: 'blog',
-  dialect: 'mysql',
-  username: 'admin',
-  password: '123124',
-  modelPaths: [__dirname + '/models']
-});
+const config = require('../../config/config');
+config.modelPaths = [__dirname + '/models'];
 
-console.log(__dirname + '/models')
+const sequelize = new Sequelize(config);
 
-
+console.log(__dirname + '/models');
 
 const testFetch  = async () => {
 
   const res = await sequelize.sync({force: true});
+  const sectorData = require('../../data/sector');
+  await Sector.bulkCreate(sectorData);
 }
 
 
